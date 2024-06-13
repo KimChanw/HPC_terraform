1426b34bd107e2c7c040479fd5702083af166428
chanwoo <chanwoo0628@gmail.com>
2024-06-13 21:42:56 +0900

    Create new oci tf files

---
+++ OCI/Control_PoC/vars.tf
@@ -0,0 +1,10 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+
+variable "vcn_id" {}
+variable "subnet_id" {}
+variable "instance_shape" {}
